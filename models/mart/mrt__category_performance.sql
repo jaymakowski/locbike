dc435682860b13a3324734af0a7b68be3b26689b
J M <thye.moor@gmail.com>
2025-11-03 18:05:41 +0000

    Almooost the last commit

--- models/mart/mrt__category_performance.sql
+++ models/mart/mrt__category_performance.sql
@@ -1,8 +1,10 @@
 SELECT 
     category_name,
     FORMAT_DATE('%b-%Y', DATE_TRUNC(order_date,MONTH)) as order_month,
+    DATE_TRUNC(order_date,MONTH) as order_month_first_day,
     COUNT(DISTINCT order_id) AS order_count,
     SUM(quantity) as quantity_total,
     SUM(discount_applied_order_item_total) as category_sales_total,
 FROM {{ ref('int__product_sales') }} as ps
-GROUP BY 1,2
+GROUP BY 1,2, 3
+ORDER BY order_month_first_day desc
